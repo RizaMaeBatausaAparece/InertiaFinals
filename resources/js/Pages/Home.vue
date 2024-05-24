48caa11fa2e141a677137977c0247cf87cf96251
Badilla246 <badillaangelo45@gmail.com>
2024-05-24 12:26:29 +0800

    main

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -20,12 +20,28 @@ defineOptions({
         <p class="text-center">Movie collections are essential for film enthusiasts,</p>
         <p class="text-center">providing a way to track viewed films, discover new favorites, </p>
         <p class="text-center mb-5">and organize movie marathons. </p>
+        <div class="genre">
+            <iframe width="250" height="500" src="https://www.youtube.com/embed/XwFIxxb_bg8" title="Momo - The Pool | Short Horror Film" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            <iframe width="250" height="500" src="https://www.youtube.com/embed/LYibxDWXOAo" title="8 ASSASSINS 🎬 Exclusive Full Action Sci-Fi Movie Premiere 🎬 English HD 2024" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            <iframe width="250" height="500" src="https://www.youtube.com/embed/8jX4uyylIIk" title="‘Haba Baba Doo, Puti Puti Poo’ FULL MOVIE | Babalu, Redford White" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        </div>
         <Link href="/movies" class="route font-bold bg-red-600 py-2 px-5 text-center rounded">Explore now</Link>
         </div>
     </main>
 </template>
 
 <style scoped>
+
+.genre {
+    display: flex; /* Utilizing flexbox for layout */
+    justify-content: space-between; /* Distributing items along the main axis */
+}
+
+.genre iframe {
+    width: 300px; /* Setting a fixed width for each iframe */
+    height: 300px; /* Setting a fixed height for each iframe */
+}
+
 .route{
     margin-left: 430px;
 }
@@ -35,7 +51,7 @@ defineOptions({
 .info {
     background-color: black;
     color: white;
-    height: 200vh;
+    max-height: 100%;
 }
 img {
     height: 100px;
@@ -43,4 +59,5 @@ img {
     border-radius: 50%;
     margin-left: 440px;
 }
+
 </style>
